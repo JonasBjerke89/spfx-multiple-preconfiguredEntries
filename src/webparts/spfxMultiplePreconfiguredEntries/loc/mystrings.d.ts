e655c54b2145acd57cc026621ce2ce408e334abf
jonas.v.bjerke@gmail.com <jonas.v.bjerke@gmail.com>
2016-09-08 20:15:06 +0200

    Init

---
+++ src/webparts/spfxMultiplePreconfiguredEntries/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpfxMultiplePreconfiguredEntriesStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'spfxMultiplePreconfiguredEntriesStrings' {
+  const strings: ISpfxMultiplePreconfiguredEntriesStrings;
+  export = strings;
+}
